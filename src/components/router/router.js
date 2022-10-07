c0720b2c72fd9ae06891f8844125c7e46163bcdd
mahiuddintuhin <mahiuddiinntuhin@gmail.com>
2022-10-07 07:37:26 +0600

    not working

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,21 +1,32 @@
-import { createBrowserRouter } from "react-router-dom";
-import Drinks from "../Drinks/Drinks";
-import Home from "../Home/Home";
-import Main from "./../../Layout/Main";
+import { createBrowserRouter, NavLink } from "react-router-dom";
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main></Main>,
-    children: [
-      {
-        path: "drinks",
-        element: <Drinks></Drinks>,
-      },
-    ],
+    element: (
+      <div>
+        <h1>inside home</h1>
+        <NavLink to="about" alt="">
+          about
+        </NavLink>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: (
+      <div>
+        <h1>inside about</h1>
+        <NavLink to="yoyoyu" alt="">
+          yoyoyu
+        </NavLink>
+      </div>
+    ),
   },
   {
     path: "*",
-    element: <h1>Error page 404 not found</h1>,
+    element: <h1>error 404</h1>,
   },
 ]);
+
+export { router };
