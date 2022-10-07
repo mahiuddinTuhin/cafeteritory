c6e741cbc2d9ab0a295d93b7a1339d2ebc981905
mahiuddintuhin <mahiuddiinntuhin@gmail.com>
2022-10-07 06:55:01 +0600

    simplify

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-3xl p-36 bg-rose-300">This is home section</h1>
+      <a href="drinks" alt="">
+        drinks
+      </a>
     </div>
   );
 };
