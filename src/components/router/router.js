6d978bd5df77735b241c19c6df6f271e903aa91a
mahiuddintuhin <mahiuddiinntuhin@gmail.com>
2022-10-07 06:52:00 +0600

    simplyfy

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,23 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
-import Main from "../../Layout/Main";
 import Drinks from "../Drinks/Drinks";
 import Home from "../Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main></Main>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>,
-      },
-      {
-        path: "/drinks",
-        element: <Drinks></Drinks>,
-      },
-    ],
+    element: <Home></Home>,
   },
+
+  {
+    path: "/drinks",
+    element: <Drinks></Drinks>,
+  },
+
   {
     path: "/*",
     element: <h1>Error page 404 not found</h1>,
